c90aa999b6f5e09b9aff398aa287d54eaaf95a34
pixalquarks <kashyaprakshat@gmail.com>
2022-08-02 15:29:06 +0530

    moved iceserver to config file

--- client/src/config.ts
+++ client/src/config.ts
@@ -8,4 +8,35 @@ const firebaseConfig = {
     measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
+export const servers = {
+    iceServers: [
+      {
+        urls: ['stun:stun.l.google.com:19302', 'stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302', 'stun:stun3.l.google.com:19302', 'stun:stun4.l.google.com:19302', "stun:openrelay.metered.ca:80"]
+      },
+      {
+        urls: "turn:openrelay.metered.ca:80",
+        username: "openrelayproject",
+        credential: "openrelayproject",
+      },
+      {
+        urls: "turn:openrelay.metered.ca:443",
+        username: "openrelayproject",
+        credential: "openrelayproject",
+      },
+      {
+        urls: "turn:openrelay.metered.ca:443?transport=tcp",
+        username: "openrelayproject",
+        credential: "openrelayproject",
+      },
+    ],
+    iceCandidatePoolSize: 10,
+  }
+
+export const displayMediaOptions = {
+video: {
+    cursor: "always"
+},
+audio: false
+} as MediaStreamConstraints;
+
 export default firebaseConfig;
